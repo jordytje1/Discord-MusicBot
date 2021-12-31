62cbe41fe175b6b78d6deb325c1a2243182811d9
jordytje1 <60434004+jordytje1@users.noreply.github.com>
2021-12-31 12:16:42 +0100

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -28,9 +28,9 @@ module.exports = {
   //Lavalink
   Lavalink: {
     id: "Main",
-    host: "lava.sudhan.tech",
+    host: "0.0.0.0",
     port: 1234, // The port that lavalink is listening to. This must be a number!
-    pass: "CodingWithSudhan",
+    pass: "1234",
     secure: false, // Set this to true if the lavalink uses SSL or you're hosting lavalink on repl.it
   },
 
